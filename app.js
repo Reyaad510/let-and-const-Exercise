cfbdfb32168656b51bbf983a2d647a3ff491b3b6
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-15 20:31:53 -0500

    Project completed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hi");
+// ES5 Global Constants
+
+/*
+var PI = 3.14;
+PI = 42;
+
+console.log(PI);
+*/
+
+// ES2015 Global Constants
+// Write and ES2015 Version
+
+const PI = 3.14;
+PI = 42;
